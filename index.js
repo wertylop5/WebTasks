49e0094d614ec602dcd5083b13bcc3201e2c8fdc
wertylop5 <slin55055@gmail.com>
2018-06-24 20:40:37 -0400

    html not served correctly

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-let app = require("express");
+"use strict";
+
+const express = require("express");
+
+let app = express();
+app.use(express.static("static/html"));
+/*
+app.use((res, req, next) => {
+	console.log("middle");
+	//next();
+});
+*/
+
+app.get("/", (req, res) => {
+	console.log("uhh");
+	//res.send("Hello");
+	
+	//won't work
+	//res.render("static/html/index.html");
+});
+
+app.listen(3000, () => {
+	console.log("Hi");
+});
 
