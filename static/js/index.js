b299fc7a15dbaa05af5a6afc6e5c5018686ea5ac
wertylop5 <slin55055@gmail.com>
2018-06-24 20:41:36 -0400

    added files

---
+++ static/js/index.js
@@ -0,0 +1,11 @@
+"use strict";
+const React = require("react");
+const ReactDOM = require("react-dom");
+
+class App extends React.Component {
+	render() {
+		return <h1>React!</h1>;
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
